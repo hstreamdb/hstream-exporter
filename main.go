4536e574841661d35324373fcdca1b14256c2c7b
YangKian <1207783292@qq.com>
2022-10-11 12:38:56 +0800

    feat: add logger

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/hstreamdb/hstream-exporter/collector"
+	"github.com/hstreamdb/hstream-exporter/util"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -12,10 +13,11 @@ import (
 )
 
 var (
-	httpServerAddr         = flag.String("addr", "127.0.0.1:8080", "http server addr")
-	listenAddr             = flag.String("listen-addr", ":9200", "port on which to expose metrics")
+	httpServerAddr         = flag.String("addr", "127.0.0.1:8080", "Http server addr")
+	listenAddr             = flag.String("listen-addr", ":9200", "Port on which to expose metrics")
 	disableExporterMetrics = flag.Bool("disable-exporter-metrics", false, "Exclude metrics about the exporter itself")
 	maxScrapeRequest       = flag.Int("max-request", 40, "Maximum number of parallel scrape requests. Use 0 to disable.")
+	logLevel               = flag.String("log-level", "info", "Exporter log level")
 )
 
 func newHandler(serverUrl string, includeExporterMetrics bool, maxRequests int) (http.Handler, error) {
@@ -55,6 +57,7 @@ func newHandler(serverUrl string, includeExporterMetrics bool, maxRequests int) 
 
 func main() {
 	flag.Parse()
+	util.InitLogger(*logLevel)
 	handler, err := newHandler(*httpServerAddr, !(*disableExporterMetrics), *maxScrapeRequest)
 	if err != nil {
 		panic(fmt.Sprintf("create http handler err: %s", err.Error()))
